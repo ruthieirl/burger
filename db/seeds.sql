65ad7caeda2afec3ff3aa1ced7284a34b011a0ed
ruthieirl <ruthfromjersey@gmail.com>
2017-11-03 23:47:50 -0400

    it works hallelujerrrr

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
 INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheeseburger');
-INSERT INTO burgers (burger_name) VALUES ('Italian Portabello Burger');
+INSERT INTO burgers (burger_name) VALUES ('Italian Portabella Burger');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Swiss BBQ Burger', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Avocado Burger', true);
